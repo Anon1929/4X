19df151a96a91023907088b7c271b25b2a14c7cb
Anon1929 <anon89345@outlook.com>
2021-07-14 22:07:08 -0300

    Basic Tile Functionalities Created Camera, Tile and GameMap Classes Added Simple Colision function Scrolling Rendering for tiles

--- testRender.cpp
+++ testRender.cpp
@@ -7,23 +7,23 @@ int main(){
     game.init(300, 300, "bicicleta");
 
     SDL_Texture * characterTexture = IMG_LoadTexture(game.getRenderer(), "./sprite_sheet.png" );
-    
-    Sprite * player = game.createSprite(50, 50, 128, 128, 0, 0, 32, 32, characterTexture );
+	vector<SDL_Texture*> tiletextures;
+    tiletextures.emplace_back(IMG_LoadTexture(game.getRenderer(), "tile01.png" ));
+    tiletextures.emplace_back(IMG_LoadTexture(game.getRenderer(), "tile02.png" ));
+    tiletextures.emplace_back(IMG_LoadTexture(game.getRenderer(), "tile03.png" ));
+	Sprite * player = game.createSprite(50, 50, 128, 128, 0, 0, 32, 32, characterTexture );
     game.createSprite(40, 40, 128, 128, 0, 0, 32, 32, characterTexture );
+	Camera cam(0,0,game.getScreen_Width(),game.getScreen_Height());
+	GameMap stage(600,600);
+	for (int i =0;i<400;i++){
+		stage.maptiles.emplace_back(Tile((i*TILE_WIDTH)%stage.GetWidth(),((i*TILE_WIDTH)/stage.GetWidth())*TILE_HEIGHT,i%3));
+		//cout << stage.GetMapTiles()[i].GetCoordinates().x << endl;
+	}
+
     while(game.update(20)){
         game.getEvents();
-        game.draw();
-
-        if(game.keyboard['w'] == true){
-            player->transform(0, -5);
-        }else if(game.keyboard['s'] == true){
-            player->transform(0, 5);
-        }else if(game.keyboard['a'] == true){
-            player->transform(-5, 0);
-        }else if(game.keyboard['d'] == true){
-            player->transform(5, 0);
-        }
-
+		cam.Move(game.keyboard,stage);
+        game.draw(stage,cam,tiletextures);
     }
 
     return 0;
